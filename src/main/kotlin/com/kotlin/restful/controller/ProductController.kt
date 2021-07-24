ae58c86c9991069f21ba12572cd6c498880fba29
bayubagusbagaswara <bagaszwara12@gmail.com>
2021-07-24 12:16:42 +0700

    membuat controller untuk Delete Product

--- src/main/kotlin/com/kotlin/restful/controller/ProductController.kt
+++ src/main/kotlin/com/kotlin/restful/controller/ProductController.kt
@@ -72,6 +72,20 @@ class ProductController(val productService: ProductService) {
         )
     }
 
-
+    /**
+     * controller untuk Delete Product
+     */
+    @DeleteMapping(
+        value = ["/api/products/{idProduct}"],
+        produces = ["application/json"]
+    )
+    fun deleteProduct(@PathVariable("idProduct") id: String): WebResponse<String> {
+        productService.delete(id)
+        return WebResponse(
+            code = 200,
+            status = "OK",
+            data = id
+        )
+    }
 
 }
