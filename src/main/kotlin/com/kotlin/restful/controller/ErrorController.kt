1655773b8568b3bab012047aa07cbc2c644d7d4d
bayubagusbagaswara <bagaszwara12@gmail.com>
2021-07-24 13:24:16 +0700

    membuat error handler untuk unauthorized exception

--- src/main/kotlin/com/kotlin/restful/controller/ErrorController.kt
+++ src/main/kotlin/com/kotlin/restful/controller/ErrorController.kt
@@ -1,6 +1,7 @@
 package com.kotlin.restful.controller
 
 import com.kotlin.restful.error.NotFoundException
+import com.kotlin.restful.error.UnauthorizedException
 import com.kotlin.restful.model.WebResponse
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -32,6 +33,16 @@ class ErrorController {
             code = 404,
             status = "NOT FOUND",
             data = "Not Found"
+        )
+    }
+
+    // handler error unutk UnauthorizedException
+    @ExceptionHandler(value = [UnauthorizedException::class])
+    fun unauthorized(unauthorizedException: UnauthorizedException): WebResponse<String> {
+        return WebResponse(
+            code = 401,
+            status = "UNAUTHORIZED",
+            data = "Please put your X-Api-Key"
         )
     }
 
